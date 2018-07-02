160b4fa14678f883d63327ee6a444ab58e7bb148
Zhaoxuyang <zhaoxuyang@jd.com>
2018-07-03 00:42:05 +0800

    feature: 爬取预告片

---
+++ server/crawler/video.js
@@ -0,0 +1,70 @@
+const puppeteer = require('puppeteer')
+const base = `https://movie.douban.com/subject/`
+const doubanId = '26627736'
+const videoBase = `https://movie.douban.com/trailer/231509/`
+
+const sleep = time => new Promise(resolve => {
+  setTimeout(resolve, time)
+})
+
+;(async () => {
+
+  console.log('Start visit target page')
+
+  const broswer = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  })
+
+  const page = await broswer.newPage()
+  await page.goto(base + doubanId, {
+    waitUntil: 'networkidle2'
+  })
+
+  await sleep(1000)
+
+  const result = await page.evaluate(() => {
+    const $ = window.$
+    const it = $('.related-pic-video')
+    if (it && it.length > 0) {
+      const link = it.attr('href')
+      const cover = it.attr('style').split('url(')[1].split('?')[0]
+      return {
+        link,
+        cover
+      }
+    }
+    return {}
+  })
+
+  let video
+
+  if (result.link) {
+    await page.goto(result.link, {
+      waitUntil: 'networkidle2'
+    })
+    await(2000)
+    video = await page.evaluate(() => {
+      const $ = window.$
+      const it = $('source')
+      console.log(it.attr('src'), 111111)
+      if (it && it.length > 0) {
+        return it.attr('src')
+      }
+      return ''
+    })
+  }
+
+  const data = {
+    video,
+    doubanId,
+    cover: result.cover,
+  }
+
+  broswer.close()
+
+  process.send(data)
+  process.exit(0)
+})()
+
+
---
+++ server/tasks/trailer.js
@@ -0,0 +1,34 @@
+const cp = require('child_process')
+const { resolve } = require('path')
+
+;(async () => {
+
+  const script = resolve(__dirname, '../crawler/video')
+
+  const child = cp.fork(script, [])
+
+  let invoked = false
+
+  child.on('error', err => {
+    if (invoked) return
+
+    invoked = true
+
+    console.log(err)
+  })
+
+  child.on('exit', code => {
+    if (invoked) return
+
+    invoked = true
+    const err = code === 0 ? null : new Error('exit error ' + code)
+
+    console.log(err)
+  })
+
+  child.on('message', data => {
+    console.log(data)
+  })
+
+})()
+
